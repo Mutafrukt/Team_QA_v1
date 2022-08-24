79fbfe8c3788c5f78db7f13993032a47f28a1fe3
Pavel O <pavelnitel@gmail.com>
2022-08-24 18:16:01 +0300

    Added Test

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,23 +1,54 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
+    GameStore store = new GameStore();
+    Player player = new Player("Petya");
+    Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+    Game game2 = store.publishGame("Diablo", "RPG");
+    Game game3 = store.publishGame("Warcraft 2", "Стратегия");
+    Game game4 = store.publishGame("Ведьмак 2", "RPG");
+
     @Test
     public void shouldSumGenreIfOneGame() {
-        GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-        Player player = new Player("Petya");
-        player.installGame(game);
-        player.play(game, 3);
+        player.installGame(game1);
+        player.play(game1, 3);
 
         int expected = 3;
-        int actual = player.sumGenre(game.getGenre());
+        int actual = player.sumGenre(game1.getGenre());
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldSumGenreIFNotPlay() {
+
+        player.installGame(game1);
+
+        int expected = 0;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreDifferentTwoGame() {
+
+        player.installGame(game1);
+        player.installGame(game2);
+
+        player.play(game1, 3);
+        player.play(game2, 4);
+        player.play(game2, 4);
+
+        int expected = 8;
+        int actual = player.sumGenre(game2.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
 }
