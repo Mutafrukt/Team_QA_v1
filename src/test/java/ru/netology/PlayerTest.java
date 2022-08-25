3f49cc24bb67c126d79b6b5dc28fc93dfd7c2e12
Pavel O <pavelnitel@gmail.com>
2022-08-25 21:38:15 +0300

    Added all test

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -36,6 +36,19 @@ public class PlayerTest {
     }
 
     @Test
+    public void shouldSumGenreIFInstallSeveralGameButNotPlay() {
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+
+        int expected = 0;
+        int actual = player.sumGenre(game3.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void shouldSumGenreDifferentTwoGame() {
 
         player.installGame(game1);
@@ -51,4 +64,62 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldSumGenreNotPlayWithGame() {
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.installGame(game4);
+
+        player.play(game4, 2);
+        player.play(game2, 1);
+        player.play(game3, 5);
+
+        int expected = 0;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void goOutRuntimeExceptionNotInstallGame() {
+
+        player.installGame(game1);
+        player.installGame(game2);
+
+        assertThrows(RuntimeException.class, () -> player.play(game3, 1));
+    }
+
+    @Test
+    public void shouldGameMostPlayerByGenre() {
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.installGame(game4);
+
+        player.play(game3, 5);
+        player.play(game2, 4);
+
+        Game expected = game3;
+        Game actual = player.mostPlayerByGenre("Стратегия");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGameMostPlayerByGenreNotPlay() {
+
+        player.installGame(game3);
+        player.installGame(game4);
+
+        player.play(game3, 5);
+        player.play(game4, 4);
+
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre("Аркады");
+
+        assertEquals(expected, actual);
+    }
 }
